9f735bdaf42cba6a5003b5a2560936d365eb992a
willlka <yana.ufa@gmail.com>
2018-01-12 16:59:07 +0500

    Показ/скрытие надписи пауза при собственно паузе

--- Assets/Scripts/Game/Pauser.cs
+++ Assets/Scripts/Game/Pauser.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Pauser : MonoBehaviour {
-	private bool paused = false;
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyUp(KeyCode.P))
-		{
-			paused = !paused;
-		}
+public class Pauser : MonoBehaviour
+{
+    public GameObject PauseGUI;
 
-		if(paused)
-			Time.timeScale = 0;
-		else
-			Time.timeScale = 1;
-	}
+    private bool paused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            paused = !paused;
+
+            if (paused)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+
+            PauseGUI.SetActive(paused);
+        }
+    }
 }
