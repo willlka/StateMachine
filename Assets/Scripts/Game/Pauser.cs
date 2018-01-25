8b2c9cdbca2a2cff39a34bb5df710c649136039b
willlka <yana.ufa@gmail.com>
2018-01-25 14:42:31 +0500

    Рефакторинг скрипта Pause

--- Assets/Scripts/Game/Pauser.cs
+++ Assets/Scripts/Game/Pauser.cs
@@ -6,17 +6,12 @@ public class Pauser : MonoBehaviour
 
     private bool paused = false;
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyUp(KeyCode.P))
         {
             paused = !paused;
-
-            if (paused)
-                Time.timeScale = 0;
-            else
-                Time.timeScale = 1;
-
+            Time.timeScale = paused ? 0 : 1;
             PauseGUI.SetActive(paused);
         }
     }
